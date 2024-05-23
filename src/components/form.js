1bf67a597e1e7004820ae13df38ec42780cce9fa
maiabrr <maiabelenr@gmail.com>
2024-05-22 23:02:25 -0400

    mas formularios

--- src/components/form.js
+++ src/components/form.js
@@ -8,11 +8,52 @@ const Form = () =>{
         password: '',
         confirmPassword: '',
     });
+    const [errors, setErrors] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
     const handleChange = (e) => {
         const {name, value} = e.target;
         setFormData({
             ...formData,
             [name]: value,
+        });
+        validateField(name, value);
+    };
+    const validateField = (name, value) =>{
+        let error = '';
+        switch (name){
+            case 'firstName':
+            case 'lastName':
+                if(value.length <2){
+                    error= 'This field must be at least 2 characters long';
+                }
+                break;
+            case 'email':
+                if(value.length < 5){
+                    error= 'This field must be at least 5 characters long';
+                }
+                break;
+            case 'password':
+                if(value.length < 8){
+                    error= 'Password must be at least 8 characters long';
+                }
+                break;
+            case 'confirmPassword':
+                if (value !== formData.password){
+                    error = 'Password do not match';
+                }
+                break;
+                default:
+                break;
+
+        }
+        setErrors({
+            ...errors,
+            [name]: error,
         });
     };
     return(
@@ -27,6 +68,7 @@ const Form = () =>{
                     name='firstName'
                     value={formData.firstName} 
                     onChange={handleChange}/>
+                    {errors.firstName && <span className='error'>{errors.firstName}</span>}
                 </div>
                 <div>
                     <label>
@@ -36,6 +78,7 @@ const Form = () =>{
                     name='lastName'
                     value={formData.lastName}
                     onChange={handleChange}/>
+                    {errors.lastName && <span className='error'>{errors.lastName}</span>}
                 </div>
                 <div>
                     <label>
@@ -45,6 +88,7 @@ const Form = () =>{
                     name='email'
                     value={formData.email}
                     onChange={handleChange}/>
+                    {errors.email && <span className='error'>{errors.email}</span>}
                 </div>
                 <div>
                     <label>
@@ -54,6 +98,7 @@ const Form = () =>{
                     name='password'
                     value={formData.password}
                     onChange={handleChange}/>
+                    {errors.password && <span className='error'>{errors.password}</span>}
                 </div>
                 <div>
                     <label>
@@ -63,6 +108,7 @@ const Form = () =>{
                     name='confirmPassword'
                     value={formData.confirmPassword}
                     onChange={handleChange}/>
+                    {errors.confirmPassword && <span className='error'>{errors.confirmPassword}</span>}
                 </div>
                 <div className='form-data-display'>
                     <h3>Your Form Data</h3>
